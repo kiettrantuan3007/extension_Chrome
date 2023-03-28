a1fba38f51c0d8a2fbd72d93a1211cc2f56fe0d1
kiettrantuan3007 <kiet.trantuan3007@gmail.com>
2023-03-28 17:29:24 +0700

    update the icon sound

--- main.js
+++ main.js
@@ -1,22 +1,13 @@
 const extensionStyle = document.createElement("link")
 extensionStyle.rel = "stylesheet"
 extensionStyle.href = "./extension_style.css"
-const extensionFavicon = document.createElement("script")
-extensionFavicon.src = "https://kit.fontawesome.com/d0bc29860c.js"
-extensionFavicon.crossOrigin = "anonymous"
 document.querySelector("head").append(extensionStyle)
-document.querySelector("body").append(extensionFavicon)
-
 function killAllPopup() {
     document.getElementById("extensionPopUp").outerHTML = "";
 }
-
-
 document.addEventListener('mouseup', async (value) => {
-
     if (document.getElementById("extensionPopUp")) {
         if (value.toElement.id == "extensionPopUp" || value.toElement.id == "buttonPlay" || value.toElement.id == "inExtension") {
-
         } else {
             killAllPopup();
         }
@@ -27,7 +18,7 @@ document.addEventListener('mouseup', async (value) => {
     else {
         const selectTag = document.getSelection().getRangeAt(0).commonAncestorContainer.parentElement
         selectTag.style.position = "relative"
-        const dataSearch = document.getSelection().toString()
+        var dataSearch = document.getSelection().toString()
         requestString = `https://api.dictionaryapi.dev/api/v2/entries/en/${dataSearch}`
         const para = document.createElement("div");
         para.id = "extensionPopUp"
@@ -40,9 +31,12 @@ document.addEventListener('mouseup', async (value) => {
         var translated = await dich.json()
         translated = translated.data.translations[0].translatedText
 
-        // console.log(translated)
+        console.log(translated)
         await fetch(requestString)
-            .then(res => res.json())
+            .then(res => {
+                if (res.status != 200) throw Error("Word not found!")
+                return res.json()
+            })
             .then(value => {
                 if (value[0].word) word = value[0].word;
                 if (value[0].phonetics) phonetics = value[0].phonetics;
@@ -51,12 +45,7 @@ document.addEventListener('mouseup', async (value) => {
                 <p>${translated}</p>`
                 for (let phonetic of phonetics) {
                     if (!phonetic.text) continue
-                    else { addHTML = addHTML + `<span id="inExtension">${phonetic.text}    </span>` }
-                    if (!phonetic.audio) continue
-                    else {
-                        addHTML = addHTML + `<a id="buttonPlay" onclick="this.firstChild.play()"><audio src="${phonetic.audio}"></audio><i id="buttonPlay" class="fa-solid fa-play"></i></a>
-                    `}
-
+                    else { addHTML = addHTML + `<a id="inExtension" ${(phonetic.audio) ? 'style = "color: blue"' : ''} onclick="this.firstChild.play()"><audio src="${(phonetic.audio) ? phonetic.audio : ""}"></audio>${phonetic.text} </a>` }
                 }
                 for (let meaning of meanings) {
                     addHTML = addHTML + `
@@ -67,15 +56,12 @@ document.addEventListener('mouseup', async (value) => {
                 addHTML = addHTML + '<p id="inExtension" class="thank">API thank to Free Dictionary API </p>'
             })
             .catch(e => {
-                if (addHTML == "") addHTML = addHTML + `${translated}`
-                console.log(e)
+                if (addHTML == "") {
+                    addHTML = addHTML + `<h3 id="inExtension">${dataSearch}</h3><br>${translated}`
+                }
             })
 
         document.querySelector("#extensionPopUp").innerHTML = addHTML
     }
 
 })
-
-
-
-// translate api: POST/ https://translation.googleapis.com/language/translate/v2?q=hello&target=vi&key=AIzaSyAT-ZHseAa2b3C5Mtc-T46v4jSHhifBCKc
