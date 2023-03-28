0bc28f4e57a14b40d0a5720c7d6f2765fbc9f1bf
kiettrantuan3007 <kiet.trantuan3007@gmail.com>
2023-03-28 14:07:59 +0700

    add google translator api

--- main.js
+++ main.js
@@ -13,9 +13,7 @@ function killAllPopup() {
 
 
 document.addEventListener('mouseup', async (value) => {
-    console.log(value.toElement)
-    console.log(value.toElement.class)
-    console.log(value.toElement.id)
+
     if (document.getElementById("extensionPopUp")) {
         if (value.toElement.id == "extensionPopUp" || value.toElement.id == "buttonPlay" || value.toElement.id == "inExtension") {
 
@@ -25,9 +23,8 @@ document.addEventListener('mouseup', async (value) => {
     }
 
     const getString = document.getSelection().toString()
-    if (!getString || getString.trim().includes(" ") || getString.includes("/") || getString.includes(":") || getString.includes("_") || getString.includes("{") || getString.includes("}") || getString.includes(".") || getString.includes("=")) { return 0 }
+    if (!getString || getString.includes("/") || getString.includes(":") || getString.includes("_") || getString.includes("{") || getString.includes("}") || getString.includes(".") || getString.includes("=")) { return 0 }
     else {
-        console.log(document.getSelection())
         const selectTag = document.getSelection().getRangeAt(0).commonAncestorContainer.parentElement
         selectTag.style.position = "relative"
         const dataSearch = document.getSelection().toString()
@@ -38,15 +35,21 @@ document.addEventListener('mouseup', async (value) => {
         console.log(requestString)
         var word, phonetics, meanings;
         var addHTML = ""
+
+        const dich = await fetch(`https://translation.googleapis.com/language/translate/v2?q=${dataSearch}&target=vi&key=AIzaSyAT-ZHseAa2b3C5Mtc-T46v4jSHhifBCKc`, { method: "POST" })
+        var translated = await dich.json()
+        translated = translated.data.translations[0].translatedText
+
+        // console.log(translated)
         await fetch(requestString)
             .then(res => res.json())
             .then(value => {
-                console.log(value)
                 if (value[0].word) word = value[0].word;
                 if (value[0].phonetics) phonetics = value[0].phonetics;
                 if (value[0].meanings) meanings = value[0].meanings
-                addHTML = `<h2 id="inExtension">${word}</h2>`
-                for (phonetic of phonetics) {
+                addHTML = `<h2 id="inExtension">${word}</h2>
+                <p>${translated}</p>`
+                for (let phonetic of phonetics) {
                     if (!phonetic.text) continue
                     else { addHTML = addHTML + `<span id="inExtension">${phonetic.text}    </span>` }
                     if (!phonetic.audio) continue
@@ -55,16 +58,16 @@ document.addEventListener('mouseup', async (value) => {
                     `}
 
                 }
-                for (meaning of meanings) {
+                for (let meaning of meanings) {
                     addHTML = addHTML + `
-            <h3 id="inExtension">${meaning.partOfSpeech}</h3>
-            <p id="inExtension">${meaning.definitions[0].definition}</p>
-            `
+                        <h3 id="inExtension">${meaning.partOfSpeech}</h3>
+                        <p id="inExtension">${meaning.definitions[0].definition}</p>
+                    `
                 }
                 addHTML = addHTML + '<p id="inExtension" class="thank">API thank to Free Dictionary API </p>'
             })
             .catch(e => {
-                if (addHTML == "") addHTML = `<h1 id="inExtension">Word not found</h1>`
+                if (addHTML == "") addHTML = addHTML + `${translated}`
                 console.log(e)
             })
 
@@ -72,3 +75,7 @@ document.addEventListener('mouseup', async (value) => {
     }
 
 })
+
+
+
+// translate api: POST/ https://translation.googleapis.com/language/translate/v2?q=hello&target=vi&key=AIzaSyAT-ZHseAa2b3C5Mtc-T46v4jSHhifBCKc
